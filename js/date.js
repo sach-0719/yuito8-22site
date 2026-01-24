d14bf9ec375e5fd21ca7973011ae823f72e2bcef
sach-0719 <kenyui1357@gmail.com>
2026-01-24 19:38:05 +0900

    kinenmsg

--- js/date.js
+++ js/date.js
@@ -1,14 +1,36 @@
 window.onload = function () {
     var today = new Date();
+    const startDate = new Date(2021, 10, 14); // 2021年11月14日
 
-    // 曜日の配列（日曜=0）
+    // 年の差
+    let years = today.getFullYear() - startDate.getFullYear();
+    if (
+        today.getMonth() < startDate.getMonth() ||
+        (today.getMonth() === startDate.getMonth() && today.getDate() < startDate.getDate())
+    ) {
+        years--;
+    }
+
+    // 曜日
     const day = ["日", "月", "火", "水", "木", "金", "土"];
 
-    document.getElementById('year').textContent =
-        today.getFullYear() + "年";
+    // 年を表示
+    document.getElementById('year').textContent = "今日は" + today.getFullYear() + "年";
 
+    // 日付を表示
     document.getElementById('date').textContent =
         (today.getMonth() + 1) + "月" +
         today.getDate() + "日" +
         "(" + day[today.getDay()] + ")";
+
+    // 経過年数を表示
+    document.getElementById('syunen').textContent =
+        "今年でYUITO8-22のアカウント開設から、" + years + "周年となりました。これからもよろしくお願いします。";
+
+    // 11月14日だけカードを表示
+    if (today.getMonth() === 10 && today.getDate() === 14) { // 11月は10
+        document.getElementById("thankyou").style.display = "flex";
+    } else {
+        document.getElementById("thankyou").style.display = "none";
+    }
 };
